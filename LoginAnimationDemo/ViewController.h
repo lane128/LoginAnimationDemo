f7d86eb9547fca48d7e86c382d6d4f3c5770a1d6
Wang Lei <lane128@gmail.com>
2015-08-16 03:28:14 +0800

    Initial Commit

---
+++ LoginAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginAnimationDemo
+//
+//  Created by Lane on 8/16/15.
+//  Copyright (c) 2015 Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
